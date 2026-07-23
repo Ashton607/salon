b1d77b5b2303ec05766517ab1112953504e2df89
Ashton607 <portmanashton4@gmail.com>
2026-07-23 21:01:14 +0200

    Added: navbar jsx file

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import './Navbar.css'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom';
+
 
 const Navbar = () => {
+const [menuOpen, setMenuOpen] = useState(false)
+
+const handleNavClick = (sectionId) => {
+    setMenuOpen(false)
+    if (location.pathname !== '/') {
+      navigate('/', { state: { scrollTo: sectionId } })
+    } else {
+      document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <ul>
+        <li>
+        <span onClick={() => handleNavClick('hero')}>Home</span>
+        </li>
+        <li>
+        <span onClick={() => handleNavClick('services')}>Services</span>
+        </li>
+        <li>
+        <RouterLink to="/about" onClick={() => setMenuOpen(false)}>About Us</RouterLink>
+        </li>
+        <li>
+        <span onClick={() => handleNavClick('services')}>Book Online</span>
+        </li>
+        <li>
+        <span onClick={() => handleNavClick('contact')}>Contact</span>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
