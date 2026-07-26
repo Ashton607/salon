686c5fdd026d3b3faeb7e544b77df0eb2f58bc4d
Ashton607 <portmanashton4@gmail.com>
2026-07-26 13:25:29 +0200

    Added: testimonial page

---
+++ src/components/testimonial/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className='Testimonials'>
+    <div className="testimonial-content">
+    
+    </div>
+    </div>
+  )
+}
+
+export default Testimonials
